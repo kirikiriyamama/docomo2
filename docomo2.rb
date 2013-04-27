e475b542f51df774a610a64bd52651702664aa96
remain <ponde.ponde.pond@gmail.com>
2013-04-28 01:12:48 +0900

    edit docomo2.rb

--- docomo2.rb
+++ docomo2.rb
@@ -6,10 +6,14 @@ require File.join(__dir__, 'lib', 'diff')
 require File.join(__dir__, 'lib', 'send_mail')
 require File.join(__dir__, 'lib', 'Hash')
 
+
 ENVIRONMENT = 'production'
 
 DATA_PATH = File.join(__dir__, 'data')
 TEMP_DATA_PATH = File.join(__dir__, 'tmp', 'data')
+
+EMAIL_CONFIG = File.join(__dir__, 'config', 'email_delivery.yml')
+EMAIL_SUBJECT = 'docomo product update information updated'
 
 URL = 'http://www.nttdocomo.co.jp/support/utilization/product_update/list/index.html?s=date'
 
@@ -23,7 +27,6 @@ unless File.exists?(DATA_PATH)
   exit
 end
 
-subject = 'Docomo product update information updated'
 body = diff(DATA_PATH, TEMP_DATA_PATH)
 if body.nil?
   FileUtils.rm(TEMP_DATA_PATH)
@@ -31,4 +34,4 @@ if body.nil?
 end
 
 FileUtils.mv(TEMP_DATA_PATH, DATA_PATH)
-send_mail(File.join(__dir__, 'config', 'email_delivery.yml'), subject, body)
+send_mail(EMAIL_CONFIG, EMAIL_SUBJECT, body)
